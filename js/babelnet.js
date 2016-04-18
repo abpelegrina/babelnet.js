241ce6a02039d84dc949c08f3aadac2b75c42b74
abpelegrina <abpelegrina@ugr.es>
2016-04-18 13:35:04 +0200

    Added the base javascript files and the example html file

---
+++ js/babelnet.js
@@ -0,0 +1,62 @@
+var babel = new BabelNet('605862f1-fb95-4658-9cb1-53abf1b4e22a');
+
+
+function BabelNet(key){
+    this.KEY = key,
+
+    this.baseURL = 'https://babelnet.io/v3/';
+
+    this.getSynsetURL = 'getSynset';
+    this.getSynsetIdsURL =  'getSynsetIds';
+    this.getVersionURL = 'getVersion';
+    this.getSensesURL = 'getSenses';
+    this.getEdgesURL = 'getEdges';
+    this.babelfyURL = 'https://babelfy.io/v1/disambiguate';
+};
+
+/**
+*/
+BabelNet.prototype.getSynset = function(id,lang){
+    var params = {
+        'id'  : id,
+        'key' : this.KEY,
+        'filterLangs' : lang
+    };
+    return $.getJSON(this.baseURL + this.getSynsetURL + "?", params);
+};
+
+
+BabelNet.prototype.getSynsetGlosses = function(id,lan, callback){
+    this.getSynset(id,lan).done(function(response){
+        $.each(response['glosses'], function (key, val){callback(key, val)});
+    });
+};
+
+BabelNet.prototype.getSynsetIds = function (word,lang){
+    var params = {
+        'word': word,
+        'langs': lang,
+        'key' : this.KEY
+    };
+    return $.getJSON(this.baseURL + this.getSynsetIdsURL + "?", params);
+}
+
+BabelNet.prototype.getEdges = function(id){
+    var params = {
+        'id':id,
+        'key': babel.KEY
+    };
+
+    return $.getJSON(this.baseURL + this.getEdgesURL + "?", params);    
+}
+
+BabelNet.prototype.disambiguate = function(text, lang, match='EXACT_MATCHING'){
+    var params = {
+        'text' : text,
+        'lang' : lang,
+        'key'  : this.KEY
+    };
+
+    return $.getJSON(this.babelfyURL + "?", params);
+}
+
---
+++ js/use.js
@@ -0,0 +1,122 @@
+/*
+ * This file contains a few examples of how to use the wrapper.
+ * Please note that to use this example you need an API key provided by BabelNet
+ */
+
+
+// initi
+
+
+
+/**
+ * 
+ */
+function showGlossesForTerm(babel, word, lang, container){
+    container.html('');
+
+    $('<h2>Synsets (concepts) denoted by the word: "'+word+'"</h2>').appendTo(container);
+
+    babel.getSynsetIds(word,lang).done(function(response){
+        $.each(response, function(key, val) {
+           
+            babel.getSynset(val['id'],lang).done(function(response){
+
+                $('<h3>', {html:val['id']}).appendTo(container);
+
+                var entry = '';
+                $.each(response['senses'], function(key,value){
+                    entry += value['lemma'] + ', ';
+                })
+
+                entry = entry.replace(/,\s*$/, "");
+                $('<em>',{html:entry}).appendTo(container);
+                $('<br>').appendTo(container);
+
+                entry = '';
+                $.each(response['glosses'], function(key,value){
+                    entry += "Gloss: " + value['gloss'] + ' (' +value['language'] + ")<br/>";
+                    
+                })
+                $('<div>', {html:entry}).appendTo(container);
+                $('<hr>').appendTo(container);
+            });
+        });
+    });
+}
+
+
+function showEdgesForSynset(babel, id, container){
+    container.html('');
+
+    $('<h2>Edges for the synset: "'+id+'"</h2>').appendTo(container);
+
+    babel.getEdges(id).done(function(response){
+        $.each(response, function(key, val) {
+            var pointer = val['pointer'];
+            var relation = pointer['name'];
+            var group = pointer['relationGroup'];
+
+            //Types of relationGroup: HYPERNYM,  HYPONYM, MERONYM, HOLONYM, OTHER
+            if ((group.toLowerCase().indexOf("hypernym") > -1) || (group.toLowerCase().indexOf("hyponym") > -1) || (relation.toLowerCase().indexOf("antonym") > -1)) {
+                var entry = "Language: " + val['language']
+                    + "<br/>Target: " + val['target']
+                    + "<br/>Relation: " + relation
+                    + "<br/>Relation group: " + group + "<br/><br/>";
+                $('<div>', {html:entry}).appendTo(container);
+            }
+        });
+
+    });
+}
+
+
+function showCompoudWords4Synset(babel, id, lang, container){
+    container.html('');
+
+    $('<h2>Compound words for the synset: "'+id+'"</h2>').appendTo(container);
+
+    babel.getSynset(id,lang).done(function(response){
+        var entry = '';
+        $.each(response['lnToCompound'], function(key,value){
+            entry += value + ', ';
+        })
+
+        entry = entry.replace(/,\s*$/, "");
+        $('<em>',{html:entry}).appendTo(container);
+        $('<br>').appendTo(container);
+    });
+}
+
+//Setup the JQuery components
+$(document).ready(function (){
+	
+	var babel = new BabelNet('605862f1-fb95-4658-9cb1-53abf1b4e22a');
+	
+    $('#search').click(function(){
+        var word = $('#term').val();        
+        var lang = $('#lang').val();
+        showGlossesForTerm(babel,word,lang, $('#results'));
+    });
+
+    $('#edges').click(function(){        
+        showEdgesForSynset(babel,'bn:00016606n', $('#results'));
+    });
+
+    $('#compoundWords').click(function(){        
+        showCompoudWords4Synset(babel,'bn:00012945n', 'EN', $('#results'));
+    });
+
+    $('#term').on('keypress', function (event) {
+         if(event.which === 13){
+
+            //Disable textbox to prevent multiple submit
+            $(this).attr("disabled", "disabled");
+            var lang = $('#lang').val();
+            showGlossesForTerm(babel,$(this).val(),lang, $('#results'));
+
+            //Do Stuff, submit, etc..
+            $(this).removeAttr("disabled");
+         }
+   });
+
+});
