13764dc49bec7084be64c68338923beff95b39ff
abpelegrina <anabpel@gmail.com>
2016-04-21 12:49:03 +0200

    moved the parameters "enums" to a new javascript file

--- js/babelnet.js
+++ js/babelnet.js
@@ -13,60 +13,6 @@ function BabelNet(){
     this.babelfyURL = 'https://babelfy.io/v1/disambiguate';
 };
 
-/* PART OF SPEECH  (POS) possible values */
-var PartOfSpeech = Object.freeze({
-                        ADJECTIVE: 'ADJECTIVE', 
-                        ADVERB: 'ADVERB', 
-                        ARTICLE: 'ARTICLE', 
-                        CONJUNCTION:'CONJUNCTION', 
-                        DETERMINER:'DETERMINER', 
-                        INTERJECTION:'INTERJECTION',
-                        NOUN : 'NOUN',
-                        PREPOSITION : 'PREPOSITION',
-                        PRONOUN : 'PRONOUN',
-                        VERB : 'VERB'
-                    });
-
-/* Sources */
-var Source = Object.freeze({
-                        BABELNET: 'BABELNET', 
-                        GEONM: 'GEONM', 
-                        IWN: 'IWN', 
-                        MSTERM:'MSTERM', 
-                        OMWIKI:'OMWIKI', 
-                        OMWN:'OMWN',
-                        VERBNET : 'VERBNET',
-                        WIKI : 'WIKI',
-                        WIKIDATA : 'WIKIDATA',
-                        WIKICAT : 'WIKICAT',
-                        WIKIDIS: 'WIKIDIS', 
-                        WIKIQU: 'WIKIQU', 
-                        WIKIQUREDI: 'WIKIQUREDI', 
-                        WIKIRED:'WIKIRED', 
-                        WIKITR:'WIKITR', 
-                        WIKT:'WIKT',
-                        WIKTLB : 'WIKTLB',
-                        WN : 'WN',
-                        WNTR : 'WNTR',
-                        WONEF : 'WONEF'
-                    });
-
-/*Languages*/
-var Languages = Object.freeze({
-                        ES: 'ES', 
-                        EN: 'EN', 
-                        FR: 'FR'
-                    });
-
-
-/*Type of relations*/
-var TypeOfRelations = Object.freeze({
-                        HYPERNYM: 'HYPERNYM', 
-                        HYPONYM: 'HYPONYM', 
-                        MERONYM: 'MERONYM', 
-                        HOLONYM: 'HOLONYM',
-                        OTHER: 'OTHER', 
-                    });
 /**
  *
 */
---
+++ js/parameters.js
@@ -0,0 +1,54 @@
+/* PART OF SPEECH  (POS) possible values */
+var PartOfSpeech = Object.freeze({
+                        ADJECTIVE: 'ADJECTIVE', 
+                        ADVERB: 'ADVERB', 
+                        ARTICLE: 'ARTICLE', 
+                        CONJUNCTION:'CONJUNCTION', 
+                        DETERMINER:'DETERMINER', 
+                        INTERJECTION:'INTERJECTION',
+                        NOUN : 'NOUN',
+                        PREPOSITION : 'PREPOSITION',
+                        PRONOUN : 'PRONOUN',
+                        VERB : 'VERB'
+                    });
+
+/* Sources */
+var Source = Object.freeze({
+                        BABELNET: 'BABELNET', 
+                        GEONM: 'GEONM', 
+                        IWN: 'IWN', 
+                        MSTERM:'MSTERM', 
+                        OMWIKI:'OMWIKI', 
+                        OMWN:'OMWN',
+                        VERBNET : 'VERBNET',
+                        WIKI : 'WIKI',
+                        WIKIDATA : 'WIKIDATA',
+                        WIKICAT : 'WIKICAT',
+                        WIKIDIS: 'WIKIDIS', 
+                        WIKIQU: 'WIKIQU', 
+                        WIKIQUREDI: 'WIKIQUREDI', 
+                        WIKIRED:'WIKIRED', 
+                        WIKITR:'WIKITR', 
+                        WIKT:'WIKT',
+                        WIKTLB : 'WIKTLB',
+                        WN : 'WN',
+                        WNTR : 'WNTR',
+                        WONEF : 'WONEF'
+                    });
+
+/*Languages*/
+var Languages = Object.freeze({
+                        ES: 'ES', 
+                        EN: 'EN', 
+                        FR: 'FR'
+                    });
+
+
+/*Type of relations*/
+var TypeOfRelations = Object.freeze({
+                        HYPERNYM: 'HYPERNYM', 
+                        HYPONYM: 'HYPONYM', 
+                        MERONYM: 'MERONYM', 
+                        HOLONYM: 'HOLONYM',
+                        OTHER: 'OTHER', 
+                    });
